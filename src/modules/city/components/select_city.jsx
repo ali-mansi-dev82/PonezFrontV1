b7a73d2e930d78bf58aa0d6230521717b0a1fee2
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-04-23 16:56:09 +0330

    state and city modules

---
+++ src/modules/city/components/select_city.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Autocomplete, TextField } from "@mui/material";
+import { FindCityFn } from "../query";
+
+const SelectCity = ({ placeholder, onChange }) => {
+  const [cities, setCities] = useState("");
+
+  const searchCityQuery = useQuery({
+    queryKey: ["find_cities"],
+    queryFn: FindCityFn.bind(this),
+  });
+
+  useEffect(() => {
+    if (searchCityQuery?.data && searchCityQuery?.data.length > 0) {
+      setCities(
+        searchCityQuery?.data?.map((value) => {
+          return { label: value.name };
+        })
+      );
+    }
+  }, [searchCityQuery?.data]);
+
+  return (
+    <Autocomplete
+      loading
+      disablePortal
+      fullWidth={true}
+      options={cities}
+      onChange={(e, value) => {
+        onChange(value.label);
+      }}
+      sx={{ width: 300 }}
+      renderInput={(params) => (
+        <TextField placeholder={placeholder} fullWidth {...params} />
+      )}
+    />
+  );
+};
+
+export default SelectCity;
