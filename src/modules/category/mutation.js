bfac3fa5998680d36334734ee022b804776ead42
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-04-23 16:38:41 +0330

    add categories

---
+++ src/modules/category/mutation.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { API_CATEGORY_URL } from "../../config";
+
+export const CreateCategoryFn = async (data) => {
+  return await axios.post(`${API_CATEGORY_URL}/create`, data);
+};
+export const UpdateCategoryFn = async (id) => {
+  return await axios.put(`${API_CATEGORY_URL}/update/${id ?? ""}`);
+};
+// export const DeleteCategoryFn = async (id) => {
+//   return await axios.delete(`${API_CATEGORY_URL}/delete/${id ?? ""}`);
+// };
