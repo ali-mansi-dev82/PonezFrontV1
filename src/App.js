03d4a97ea6dc6f02ad5019e941af1e94a4be92d1
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-04-23 17:27:06 +0330

    add auth middleware and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Index from "./modules";
+import { Routes, Route } from "react-router-dom";
+import Index from "./modules/index";
+import Posts from "./modules/post/pages/filter/index";
+import NewPost from "./modules/post/pages/create/index";
+import Post from "./modules/post/pages/find_one/index";
+import AuthGuard from "./middleware/AuthGuard";
+import TestMui from "./modules/test/index";
+//panel
+import MyPost from "./modules/my_panel/pages/my_post";
+import MySaved from "./modules/my_panel/pages/my_saved";
+import MyNotes from "./modules/my_panel/pages/my_notes";
+import MyRecent from "./modules/my_panel/pages/recent";
+import EditPost from "./modules/my_panel/pages/edit_post";
+//errors
+import NotFound from "./modules/error/pages/not_found";
 
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
+      <Route path="/s/" element={<Posts />} />
+      <Route path="/s/:slug" element={<Posts />} />
+      <Route path="/v/:slug" element={<Post />} />
+      <Route path="/new" element={<AuthGuard component={<NewPost />} />} />
+      <Route path="/my-panel">
+        <Route path="my-post" element={<AuthGuard component={<MyPost />} />} />
+        <Route path="my-post/edit/:slug" element={<AuthGuard component={<EditPost />} />} />
+        <Route path="saved" element={<AuthGuard component={<MySaved />} />} />
+        <Route path="notes" element={<AuthGuard component={<MyNotes />} />} />
+        <Route path="recent" element={<AuthGuard component={<MyRecent />} />} />
+      </Route>
+      <Route path="/test" element={<TestMui />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
-}
+};
+
 export default App;
