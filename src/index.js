0cd9eb4d5156b5e38553f5f172a158f5c80f6044
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-04-23 18:01:53 +0330

    fix bus

--- src/index.js
+++ src/index.js
@@ -4,26 +4,27 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext";
 import { CityProvider } from "./context/CityContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProvider from "./context/MuiContext";
 
 const queryClient = new QueryClient({});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <CityProvider>
-          <AuthProvider>
-            <BrowserRouter>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <CityProvider>
+            <AuthProvider>
               <App />
-            </BrowserRouter>
-          </AuthProvider>
-        </CityProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+            </AuthProvider>
+          </CityProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 reportWebVitals();
